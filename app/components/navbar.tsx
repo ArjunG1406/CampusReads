bca0b059595b9c3454e0fe7f8ba452f75ec794cb
Arjun G <145785970+ArjunG1406@users.noreply.github.com>
2026-02-10 18:42:30 +0530

    Logo fix

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,9 +8,9 @@ const navbar = () => {
   return (
     <>
       {/* <div>{isOpen ? "Menu Open" : "Menu Closed"}</div> Use isOpen to avoid unused variable warning */}
-     <nav className='flex border-b-[0.01rem] border-zinc-700 justify-between fixed left-0 right-0 z-999 bg-black items-center h-96px sm:h-100px md:h-120px shadow-md shadow-yellow-100'>
+     <nav className='flex border-b-[0.01rem] border-zinc-700 justify-between fixed left-0 right-0 z-999 bg-black items-center h-96px sm:h-100px md:h-120px shadow-md shadow-yellow-900'>
     <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-                <img src="/favicon.ico" alt="logo" className='' />
+                <img src="/favicon.ico" alt="logo" className='h-[64px] w-auto sm:h-[66px] md:h-[80px] ps-8 sm:ps-10 md:ps-12 brightness-0 invert' />
             </FadeContent>
             <button 
                     type='button' 
